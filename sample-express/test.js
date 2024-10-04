294063e0b2304d0fb18945465b09af601780815a
Leo Salazar <leosalazar137@gmail.com>
2024-10-04 00:46:34 -0400

    silliness

---
+++ sample-express/test.js
@@ -0,0 +1,6 @@
+const originalArray = [1,2,3]
+const clonedArray = [...originalArray]
+clonedArray[0] = 'one'
+clonedArray[1] = 'two'
+clonedArray[2] = 'three'
+console.log(`originalArray: ${originalArray}, clonedArray: ${clonedArray}`)
